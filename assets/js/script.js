1e94b9e098c1ea59f37847e9ebb1f20e0425bb16
Phil Smith <smithphil868@gmail.com>
2024-08-26 13:26:32 +0000

    Add username input function and add more stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("heelo world")
+var username;
+
+document.getElementById("user-submit").onclick = function() {
+    username = document.getElementById("username").value;
+    document.getElementById("welcome-message").textContent = `Welcome ${username}, are you ready to play? Click the button below to choose your game mode.`
+    document.getElementById('next-choose-container').classList.remove('hidden');
+}
+document.getElementById('game-choices-btn').addEventListener('click', gameType);
+
+function gameType () {
+    document.getElementById('rules-container').classList.add('hidden');
+    document.getElementById('game-choices-container').classList.remove('hidden');
+    document.getElementById('game-choices-btn').classList.add('hidden');
+}
