085d51fef4d8e30ee2c4b66934b2a23710f684d4
Phil Smith <smithphil868@gmail.com>
2024-08-27 13:24:31 +0000

    add stylings to question box and add answer boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+var quizData1 = [
+    {
+        question: "/images/ger94.webp",
+        options: ["Germany", "Belgium", "England", "Denmark"],
+        correct: "Germany",
+    },
+    {
+        question: "/images/Marseille1990-91Home.png",
+        options: ["Lyon", "PSG", "Marseille", "Montpellier"],
+        correct: "Marseille",
+    },
+    {
+        question: "/images/Newcastle 91.png",
+        options: ["West Ham", "Norwich", "Newcastle", "Ipswich"],
+        correct: "Newcastle",
+    },
+    {
+        question: "/images/Nigeria1994WorldCupAway.png",
+        options: ["Cameroon", "Nigeria", "Ivory Coast", "South Africa"],
+        correct: "Nigeria",
+    },
+    {
+        question: "/images/Wolves 97.png",
+        options: ["Swindon", "West Brom", "Oldham", "Wolves"],
+        correct: "Wolves",
+    },
+];
+
 var username;
 
 document.getElementById("user-submit").onclick = function() {
@@ -11,4 +39,17 @@ function gameType () {
     document.getElementById('rules-container').classList.add('hidden');
     document.getElementById('game-choices-container').classList.remove('hidden');
     document.getElementById('game-choices-btn').classList.add('hidden');
+}
+
+document.getElementById('guess-shirt-start-btn').addEventListener('click', startGuessShirtTeam);
+document.getElementById('guess-year-start-btn').addEventListener('click', startGuessShirtYear);
+
+function startGuessShirtTeam () {
+    document.getElementById('game-choices-container').classList.add('hidden');
+    document.getElementById('question-container').classList.remove('hidden');
+}
+
+function startGuessShirtYear () {
+    document.getElementById('game-choices-container').classList.add('hidden');
+    document.getElementById('question-container').classList.remove('hidden');
 }
